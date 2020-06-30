61d40b5eaf8b9f4bf61d03ca0a44f76cea40bc4a
Hayden Bryant <haydenbryant.student@careerdevs.com>
2020-06-30 11:46:53 -0400

    user registration setup for jpa

--- src/main/java/com/webencyclop/demo/controller/AuthenticationController.java
+++ src/main/java/com/webencyclop/demo/controller/AuthenticationController.java
@@ -1,14 +1,23 @@
 package com.webencyclop.demo.controller;
 
+import com.webencyclop.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.webencyclop.demo.model.User;
 
+import javax.validation.Valid;
+
 @Controller
 public class AuthenticationController {
+
+    @Autowired
+    UserService userService;
 
     @RequestMapping(value = { "/login" }, method = RequestMethod.GET)
     public ModelAndView login() {
@@ -30,6 +39,20 @@ public class AuthenticationController {
     public ModelAndView home() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("home"); // resources/template/home.html
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ModelAndView registerUser(@Valid User user, BindingResult bindingResult, ModelMap modelMap) {
+        ModelAndView modelAndView = new ModelAndView();
+        if(bindingResult.hasErrors()){
+            modelAndView.addObject("successMessage", "Please correct errors in form");
+            modelMap.addAttribute("bindingResult", bindingResult);
+        } else if(userService.isUserAlreadyPresent(user)){
+            modelAndView.addObject("successMessage", "user already exists");
+        }
+        modelAndView.addObject("user", new User());
+        modelAndView.setViewName("register"); //resources/template/register.html
         return modelAndView;
     }
 }
---
+++ src/main/java/com/webencyclop/demo/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.webencyclop.demo.repository;
+
+import com.webencyclop.demo.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    public Role findByRole(String role);
+}
